99a939b83672a6c1c46a86ef6cf660e0f8c94a98
ALLA HEMANTH VENKATA PAVAN KUMAR REDDY <ahvworkatro@gmail.com>
2019-03-09 21:23:48 +0530

    Added Login for security. Only login activity is created. Settings need to be added later.

---
+++ app/src/main/java/com/example/allakumarreddy/moneybook/LoginSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.example.allakumarreddy.moneybook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_settings);
+    }
+}
